e0fc81cecd2ed835406bdf6f4faf8c7e66d6301e
adnanhjoy <engr.adnanhossain@gmail.com>
2024-04-01 11:55:05 +0600

    add fix brand issue

--- src/components/home/topBrands/TopBrands.jsx
+++ src/components/home/topBrands/TopBrands.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { getAllBrands } from '@/lib/categories/categoriesApi';
+import Link from 'next/link';
 
 const TopBrands = async () => {
     const data = await getAllBrands();
@@ -9,13 +10,13 @@ const TopBrands = async () => {
         <div className='lg:mt-16'>
             <h1 className='text-center font-semibold text-xl md:text-3xl'>Top Brands</h1>
 
-            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4 mt-8'>
+            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-2 md:gap-4 mt-8'>
                 {
                     data?.result?.map(brand =>
-                        <div key={brand?._id} className='bg-white rounded-lg shadow-md cursor-pointer px-14 py-10 group'>
+                        <Link href={`/products?brand=${brand?.slug}`} key={brand?._id} className='bg-white rounded-lg shadow-md cursor-pointer px-8 md:px-14 py-4 md:py-6 group flex flex-col items-center justify-center'>
                             <Image quality={100} width={500} height={500} src={brand?.image} className='w-full' alt='brand' />
-                            <h1 className='text-center group-hover:text-primary mt-3'>{brand?.name}</h1>
-                        </div>
+                            <h1 className='text-center text-xs md:sm group-hover:text-primary mt-2'>{brand?.name}</h1>
+                        </Link>
                     )
                 }
             </div>
