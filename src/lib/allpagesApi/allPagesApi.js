a3d30a33a8301d4c3e3eb3825a04c47fea4c88e5
adnanhjoy <engr.adnanhossain@gmail.com>
2024-04-21 13:08:33 +0600

    add terms condition

--- src/lib/allpagesApi/allPagesApi.js
+++ src/lib/allpagesApi/allPagesApi.js
@@ -14,7 +14,22 @@ const getPrivacyPolicy = async () => {
         console.error(error)
     }
 };
+// get privacy policy
+const getTermsCondition = async () => {
+    try {
+        const res = await fetch(`${baseUrl}/termsCondition`)
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+
+        return res.json();
+    } catch (error) {
+        console.error(error)
+    }
+};
 
 export {
     getPrivacyPolicy,
+    getTermsCondition
 }
